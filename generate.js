fefc2b9eefb3fc22440a069bbc501f14cfcd1d1e
Rajat Silwal <rajatsilwal7@gmail.com>
2019-03-31 02:51:09 +0530

    added a header page

--- generate.js
+++ generate.js
@@ -1,34 +1,65 @@
 	function randomGenerate(l){
-		if (l==="generate"){
-		if(window.XMLHttpRequest)
-			{
-			var generate= new XMLHttpRequest();
-		}
-		else
-			{
-			var generate =new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		var n=document.getElementById("number").value;
-			generate.onreadystatechange=function(){
-				if(this.readyState== 4 && this.status==200)
-					{	
-						var address=(JSON.parse(this.responseText)).message;
-						console.log(address);
-						for(var j=0;j<address.length;j++){
-							
-							var img=document.createElement("img");
-							img.setAttribute("src",address[j]);
-							document.getElementById("gallary").appendChild(img);
-						}
-			 	}
+if (l==="generate"){
+if(window.XMLHttpRequest)
+{
+var generate= new XMLHttpRequest();
+}
+else
+{
+var generate =new ActiveXObject("Microsoft.XMLHTTP");
+}
+var n=document.getElementById("number").value;
+generate.onreadystatechange=function(){
+if(this.readyState== 4 && this.status==200)
+{	
+var address=(JSON.parse(this.responseText)).message;
+console.log(address);
+for(var j=0;j<address.length;j++){
 
-			};
-	generate.open("GET","https://dog.ceo/api/breeds/image/random/"+n,false);
-	generate.send();
-	document.getElementById("clear").style.display="table-cell";
-	}
-	else{ 
-		document.getElementById("gallary").innerHTML="";
-		document.getElementById("clear").style.display="none";
-	}
-	}
+var img=document.createElement("img");
+img.setAttribute("src",address[j]);
+document.getElementById("gallary").appendChild(img);
+}
+}
+};
+generate.open("GET","https://dog.ceo/api/breeds/image/random/"+n,true);
+generate.send();
+document.getElementById("clear").style.display="table-cell";
+}
+else{ 
+document.getElementById("gallary").innerHTML="";
+document.getElementById("clear").style.display="none";
+}
+}
+
+function convertCurrency (){
+	var URL="https://free.currencyconverterapi.com/api/v6/convert?q=USD_PHP&compact=ultra&apiKey=beaebc2cbae14d4dd01f"
+	var generate2= new XMLHttpRequest();
+		generate2.open("GET",URL,true);
+		generate2.send();
+		generate2.onreadystatechange= function (){
+
+				if(this.readyState== 4 && this.status==200)
+				{
+					var restext=JSON.parse(this.responseText);
+					console.log(restext);
+				}
+				else if(this.readyState==0)
+				{
+					console.log("initialized");
+				}
+				else if(this.readyState==1)
+				{
+					console.log("connection established");
+				}
+				else if(this.readyState==2)
+				{
+					console.log("request recived");
+				}
+				else if(this.readyState==3)
+				{
+					console.log("processing");
+				}
+
+		}
+}
--- index.php
+++ index.php
@@ -1,26 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<!-- <meta http-equiv="refresh" content="5"> -->
-	<title>cute puppies</title>
-	<link rel="stylesheet" type="text/css" href="index.css"> 
+	<title>TOOLKIT.COM</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="index.css">
 </head>
-<body class="wrapper">
+<body class=" wrapper menu-body">
 	<header class="wrapper">
-		<h3>I LOVE DOGS!!!</h3>
+	<h1>Toolkit.com</h1>
 	</header>
-		<div class="wrapper" id="gallary"></div>
-		<table>
-				<td id="clear" onclick="randomGenerate('clear')"><button onclick="randomGenerate()" >Clear</button></td>
-				<td><h2>No. of PUPPIES you want</h2></td>
-				<td><input type="number"  min="4" max="1000" id="number" value=4 name="quantity"></td>
-				<td><button onclick="randomGenerate('generate')" id="generate">Generate New</button></td>
-		</table>
+<div class="wrapper menu">
+<a href="dog.html"><button class=" menu-bt"><span>If You Love Dogs! </span><i class="fas fa-dog"></i></button></a>
+<a href=""><button class=" menu-bt"><i class="fas fa-dollar-sign"></i> Convert My Currency <i class="fas fa-rupee-sign"></i></button></a>
+<a href=""><button class=" menu-bt"><span>Am I Hacked ? </span><i class="fas fa-desktop"></i></button></a>
+<a href=""><button class=" menu-bt">Whats The Score Bro ? <i class="fas fa-chart-bar"></i></button></a>
+<a href=""><button class=" menu-bt">Wiki Country <i class="fas fa-globe"></i></button></a>
+</div>
 </body>
-
-<script type="text/javascript" src="generate.js">
-
-</script>
 </html>
